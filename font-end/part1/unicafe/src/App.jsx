ec72511433ad0d4444428be41f62a9109d062aec
Khanh Chung <khanh.d.chung@gmail.com>
2023-09-15 13:09:01 -0700

    1.8: unicafe step3

--- font-end/part1/unicafe/src/App.jsx
+++ font-end/part1/unicafe/src/App.jsx
@@ -1,13 +1,27 @@
 import { useState } from 'react'
 
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad
+  const average = all === 0 ? 0 : (good - bad) / all
+  const positive = all === 0 ? 0 : (good / all) * 100
+
+  return (
+    <div>
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
+    </div>
+  )
+}
+
 function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const all = good + neutral + bad
-  const average = all === 0 ? 0 : (good - bad) / all
-  const positive = all === 0 ? 0 : (good / all) * 100
 
   return (
     <>
@@ -17,15 +31,7 @@ function App() {
         <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
         <button onClick={() => setBad(bad + 1)}>bad</button>
       </div>
-      <div>
-        <h2>statistics</h2>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all {all}</p>
-        <p>average {average}</p>
-        <p>positive {positive} %</p>
-      </div>
+      <Statistics good={good} neutral={neutral} />
     </>
   )
 }
