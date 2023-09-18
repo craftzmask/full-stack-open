2da54ceabb85723bab49f5def72cfd8064b2b075
Khanh Chung <khanh.d.chung@gmail.com>
2023-09-18 00:58:00 -0700

    2.11: The Phonebook Step6

--- font-end/part2/phonebook/src/App.jsx
+++ font-end/part2/phonebook/src/App.jsx
@@ -1,18 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
 import SearchFilter from './components/SearchFilter'
 import AddForm from './components/AddForm'
 import PersonList from './components/PersonList'
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [name, setName] = useState('')
   const [number, setNumber] = useState('')
   const [query, setQuery] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3000/persons')
+      .then(res => setPersons(res.data))
+  }, [])
 
   const filteredPersons = persons.filter(p => {
     const pName = p.name.toLowerCase()
