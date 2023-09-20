a120c23fec5b81b9a9f656f3ed48af515a3541ae
Khanh Chung <khanh.d.chung@gmail.com>
2023-09-19 22:18:40 -0700

    3.12: Command-line database

---
+++ back-end/phonebook/mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+const len = process.argv.length
+if (!(len === 3 || len === 5)) {
+  console.log('invalid arguments')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.3ccbjv0.mongodb.net/?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (len === 5) {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4]
+  })
+
+  person.save().then(savedPerson => {
+    console.log(`Added ${savedPerson.name} number ${savedPerson.number} to phonebook`)
+    mongoose.connection.close()
+  })
+}
+
+if (len === 3) {
+  console.log('phonebook:')
+  Person.find({}).then(result => {
+    result.forEach(p => console.log(p.name, p.number))
+    mongoose.connection.close()
+  })
+}
