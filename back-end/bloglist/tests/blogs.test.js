3f5a007c3d0bfd10fed1b3d2656ab59bac9a3d12
Khanh Chung <khanh.d.chung@gmail.com>
2023-09-21 15:32:49 -0700

    4.13 Blog list expansions, step1

--- back-end/bloglist/tests/blogs.test.js
+++ back-end/bloglist/tests/blogs.test.js
@@ -126,6 +126,21 @@ test('cannot not post a blog without title or url', async () => {
     .expect('Content-Type', /application\/json/)
 })
 
+test("delete a blog by providing it's valid id", async () => {
+  const blogsAtStart = await blogsInDb()
+  const blogToDelete = blogsAtStart[0]
+
+  await api
+    .delete(`/api/blogs/${blogToDelete.id}`)
+    .expect(204)
+
+  const blogsAtEnd = await blogsInDb()
+  expect(blogsAtEnd).toHaveLength(blogsAtStart.length - 1)
+
+  const titles = blogsAtEnd.map(blog => blog.title)
+  expect(titles).not.toContain(blogToDelete.title)
+})
+
 afterAll(async () => {
   await mongoose.connection.close()
 })
