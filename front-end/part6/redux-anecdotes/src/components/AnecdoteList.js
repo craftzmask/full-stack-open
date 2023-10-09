023789017a6c36c7da81da6d2c94a32272c853f4
Khanh Chung <khanh.d.chung@gmail.com>
2023-10-08 19:03:46 -0700

    6.9 Better anecdotes, step7

--- front-end/part6/redux-anecdotes/src/components/AnecdoteList.js
+++ front-end/part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -2,7 +2,15 @@ import { useSelector, useDispatch } from 'react-redux'
 import { voteAnecdote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => {
+    return state
+      .anecdotes
+      .filter(s => s
+        .content
+        .toLowerCase()
+        .includes(state.filter.toLowerCase())
+      )
+  })
   const dispatch = useDispatch()
   const sortedAnecdotes = anecdotes.sort((a, b) => a.votes - b.votes)
 
