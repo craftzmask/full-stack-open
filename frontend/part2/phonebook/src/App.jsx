04a193a0c6ad8935e34bdcf602f23f1480e22bd6
Khanh Chung <khanh.d.chung@gmail.com>
2024-04-20 13:15:35 -0700

    2.11: The Phonebook Step 6

--- frontend/part2/phonebook/src/App.jsx
+++ frontend/part2/phonebook/src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import Filter from './components/Filter'
 import AddPersonForm from './components/AddPersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [name, setName] = useState('')
   const [number, setNumber] = useState('')
   const [filter, setFilter] = useState('')
@@ -20,6 +16,12 @@ const App = () => {
       ? p.name.toLowerCase().includes(filter.toLowerCase())
       : persons
   })
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(res => setPersons(res.data))
+  }, [])
 
   const addPerson = e => {
     e.preventDefault()
