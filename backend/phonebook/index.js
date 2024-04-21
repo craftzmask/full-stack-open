952180b914c237cec009af576fe6ebb35193e7ca
Khanh Chung <khanh.d.chung@gmail.com>
2024-04-20 19:00:17 -0700

    3.9 Phonebook backend step 9

--- backend/phonebook/index.js
+++ backend/phonebook/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const morgan = require('morgan')
 
+app.use(cors())
 app.use(express.json())
-
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
