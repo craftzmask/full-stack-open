b09aba17eda14f5d50742ad2b3686b2e272df79f
Khanh Chung <khanh.d.chung@gmail.com>
2024-04-20 22:39:01 -0700

    3.12: Command-line database

--- backend/phonebook/index.js
+++ backend/phonebook/index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const morgan = require('morgan')
 
 app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
