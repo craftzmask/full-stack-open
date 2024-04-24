88d11f7a08ba00f106f0702b29ba30e733b74383
Khanh Chung <khanh.d.chung@gmail.com>
2024-04-24 09:05:56 -0700

    4.2 Blog List, step 2

---
+++ backend/bloglist/app.js
@@ -0,0 +1,28 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch(error => {
+    logger.error(error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use(morgan('tiny'))
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
