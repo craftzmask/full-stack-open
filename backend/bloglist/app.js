2bbf5d8dacc5fd7c849dc457045078f6c9c78a6e
Khanh Chung <khanh.d.chung@gmail.com>
2024-04-30 13:04:43 -0700

    4.18: Blog List Expansion, step 6

--- backend/bloglist/app.js
+++ backend/bloglist/app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose')
 const morgan = require('morgan')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 
@@ -23,6 +24,7 @@ app.use(morgan('tiny'))
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
