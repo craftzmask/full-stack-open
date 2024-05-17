023ad45757a0145e02c63e91af7947f1d017b97f
Khanh Chung <khanh.d.chung@gmail.com>
2024-05-16 23:20:42 -0700

    6.5: Anecdotes, step 3

--- frontend/part6/redux-anecdotes/src/App.jsx
+++ frontend/part6/redux-anecdotes/src/App.jsx
@@ -1,7 +1,7 @@
 import { useSelector, useDispatch } from 'react-redux'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => state.toSorted((a, b) => b.votes - a.votes))
   const dispatch = useDispatch()
   
   const vote = (id) => {
