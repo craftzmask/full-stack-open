bd6ed3fc5646ff0b0c3be7b2bc391a4f0d8ec309
Khanh Chung <khanh.d.chung@gmail.com>
2024-05-19 16:31:47 -0700

    6.15 Anecdotes and the Backend, step 2

--- frontend/part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ frontend/part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,15 +1,17 @@
 import { useDispatch } from 'react-redux'
 import { create } from '../reducers/anecdoteReducer'
 import { set, remove } from '../reducers/notifyReducer'
+import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
 
-  const createAnecdote = e => {
+  const handleSubmit = async e => {
     e.preventDefault()
     const anecdote =  e.target.anecdote.value
     e.target.anecdote.value = ''
-    dispatch(create(anecdote))
+    const newAnecdote = await anecdoteService.createAnecdote(anecdote)
+    dispatch(create(newAnecdote))
     notify(`Added ${anecdote.content}`)
   }
 
@@ -23,7 +25,7 @@ const AnecdoteForm = () => {
   return (
     <>
       <h2>create new</h2>
-      <form onSubmit={createAnecdote}>
+      <form onSubmit={handleSubmit}>
         <div><input name="anecdote"/></div>
         <button>create</button>
       </form>
